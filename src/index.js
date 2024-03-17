0e4a69b9ec69f25a921421a4e189e929169caa29
unknown <mmaksuti21@epoka.edu.al>
2024-03-17 13:29:11 +0100

    Folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const db = require("./sequelize/models")
+
 app.listen(5000, () => {
     console.log(`Server is running on port 5000`);
 })
